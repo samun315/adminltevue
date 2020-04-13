f562cab391b72a16783ced5ce22930d1e55f98b6
Samun Chowdhury <samun315@gmail.com>
2020-04-13 16:30:47 +0600

    test

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -81,13 +81,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <ul class="nav nav-treeview">
               <li class="nav-item">
                 <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="fas fa-circle nav-icon"></i>
                   <p>Active Page</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
+                  <i class="fas fa-circle nav-icon"></i>
                   <p>Inactive Page</p>
                 </a>
               </li>
